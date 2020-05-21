a63c3a60df6b51bb670b297f5e8946a0fc007cba
pikasuke <tongfongctm@hotmail.com>
2020-05-21 03:55:33 +0200

    Finalisation du Login UI transition form

--- UXLoginDesign/app/src/main/java/com/example/uxlogindedign/MainActivity.java
+++ UXLoginDesign/app/src/main/java/com/example/uxlogindedign/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.uxlogindedign;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageButton btRegister;
+    private TextView tvLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btRegister = findViewById(R.id.btRegister);
+        tvLogin = findViewById(R.id.tvLogin);
+        btRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                Pair[] pairs = new Pair[1];
+                pairs[0] = new Pair<View,String>(tvLogin, "tvLogin");
+                ActivityOptions activityOptions = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this, pairs);
+                startActivity(intent);
+            }
+        });
     }
 }
--- UXLoginDesign/app/src/main/java/com/example/uxlogindedign/RegisterActivity.java
+++ UXLoginDesign/app/src/main/java/com/example/uxlogindedign/RegisterActivity.java
@@ -1,14 +1,45 @@
 package com.example.uxlogindedign;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.RelativeLayout;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private RelativeLayout rlayout;
+    private Animation animation;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Toolbar toolbar = findViewById(R.id.bgHeader);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        rlayout = findViewById(R.id.rlayout);
+        animation = AnimationUtils.loadAnimation(this, R.anim.uptodown);
+        rlayout.setAnimation(animation);
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+            onBackPressed();
+            return true;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
